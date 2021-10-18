066bfeb543ede7cfc7f430861d01789f339ecc93
Jeroen Pfeil <jeroen.pfeil@automattic.com>
2021-10-18 21:36:39 +0700

    Fix unit tests and add README content
    Running unit tests for this repository was not easy. Using wp-env makes it straightforward and works the same way in every environment.

--- phpunit/bootstrap.php
+++ phpunit/bootstrap.php
@@ -2,18 +2,37 @@
 /**
  * PHPUnit bootstrap file
  *
- * @package Wpx_Generator
+ * @package WXR_Generator
  */
+
+
+// Require composer dependencies.
+require_once dirname( __DIR__ ) . '/vendor/autoload.php';
+
+
+// If we're running in WP's build directory, ensure that WP knows that, too.
+if ( 'build' === getenv( 'LOCAL_DIR' ) ) {
+	define( 'WP_RUN_CORE_TESTS', true );
+}
 
 $_tests_dir = getenv( 'WP_TESTS_DIR' );
 
+// Next, try the WP_PHPUNIT composer package.
 if ( ! $_tests_dir ) {
-	$_tests_dir = rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
+	$_tests_dir = getenv( 'WP_PHPUNIT__DIR' );
 }
 
-if ( ! file_exists( $_tests_dir . '/includes/functions.php' ) ) {
-	echo "Could not find $_tests_dir/includes/functions.php, have you run bin/install-wp-tests.sh ?" . PHP_EOL; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-	exit( 1 );
+// See if we're installed inside an existing WP dev instance.
+if ( ! $_tests_dir ) {
+	$_try_tests_dir = __DIR__ . '/../../../../../tests/phpunit';
+	if ( file_exists( $_try_tests_dir . '/includes/functions.php' ) ) {
+		$_tests_dir = $_try_tests_dir;
+	}
+}
+
+// Fallback.
+if ( ! $_tests_dir ) {
+	$_tests_dir = '/tmp/wordpress-tests-lib';
 }
 
 // Give access to tests_add_filter() function.
@@ -23,10 +42,14 @@ require_once $_tests_dir . '/includes/functions.php';
  * Manually load the plugin being tested.
  */
 function _manually_load_plugin() {
-	require_once dirname( dirname( __FILE__ ) ) . '/wxr-generator.php';
-	require_once dirname( dirname( __FILE__ ) ) . '/lib/class-buffer-writer.php';
+	require_once dirname( __DIR__ ) . '/wxr-generator.php';
+	require_once dirname( __DIR__ ) . '/lib/class-buffer-writer.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
+
+
 // Start up the WP testing environment.
 require $_tests_dir . '/includes/bootstrap.php';
+
+
